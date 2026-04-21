9b422d964cdc471cd3934a5f773791488e933794
Saybal <roysaybal@gmail.com>
2026-04-21 12:06:24 +0600

    Initialization

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>This is from index page</h1>
+      <h1>This is from index</h1>
       
     </div>
   );
